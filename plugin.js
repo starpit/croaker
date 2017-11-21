cf2c418a5a72b3cdfd50d3b246a87f1db5a276db
Nick M. Mitchell <nickm@us.ibm.com>
2017-11-21 17:43:32 -0500

    add croak delete

--- plugin.js
+++ plugin.js
@@ -17,7 +17,7 @@
 const { lt, end } = require('./lib/croak'),
       { last, show } = require('./lib/replay'),
       { list } = require('./lib/history-table'),
-      { clear } = require('./lib/history')
+      { del, clear } = require('./lib/history')
 
 module.exports = (commandTree, prequire) => {
     const wsk = prequire('/ui/commands/openwhisk-core')
@@ -26,6 +26,7 @@ module.exports = (commandTree, prequire) => {
     commandTree.listen('/croak/end', end)            // end any ongoing load run
     commandTree.listen('/croak/last', last)          // show last run, from history
     commandTree.listen('/croak/history', list(wsk))  // show recent history
+    commandTree.listen('/croak/delete', del)         // delete selected historical run
     commandTree.listen('/croak/clear', clear)        // clear history
     commandTree.listen('/croak/show', show)          // show a given historical run
 }
